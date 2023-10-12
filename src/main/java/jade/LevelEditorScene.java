9c6c31043749ff06acb49d546f466002e0f59445
fynnj <fynnjuranek@web.de>
2023-10-12 15:47:25 +0200

    initial commit

---
+++ src/main/java/jade/LevelEditorScene.java
@@ -0,0 +1,17 @@
+package jade;
+
+import java.awt.event.KeyEvent;
+
+public class LevelEditorScene extends Scene {
+
+    public LevelEditorScene() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+
+}
